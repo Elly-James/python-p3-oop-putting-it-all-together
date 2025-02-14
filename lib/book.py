409aef06966bc293e837236c5f06af83e6778c28
Elly <ellyjames1999@gmail.com>
2025-02-14 15:40:15 +0300

    done all tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,56 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title="River and the Source", page_count=90):
+     
+        if isinstance(title, str) and 1 <= len(title) <= 100:
+            self._title = title
+        else:
+            print("Title must be string between 1 and 100 characters.")
+            self._title = "Unknown"
+
+        if isinstance(page_count, int) and page_count > 0:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+            self._page_count = 1
+
+    def get_title(self):
+        return self._title
+
+    def set_title(self, title):
+        if isinstance(title, str) and 1 <= len(title) <= 100:
+            self._title = title
+        else:
+             print("Title must be string between 1 and 100 characters.")
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+       if isinstance(page_count, int) and page_count> 0:
+            self._page_count = page_count
+       else:
+            print("page_count must be an integer")
+            self._page_count = "Unknown"
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+   
+    title = property(get_title, set_title)
+    page_count = property(get_page_count, set_page_count)
+
+book = Book()
+print(book.title)  
+print(book.page_count)  
+
+book.title = "Rich Dad Poor Dad"
+book.page_count = 200
+print(book.title)
+print(book.page_count)
+
+book.title = "Dholuo Mit"
+book.page_count = -30
+print(book.title)
+print(book.page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,59 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand="Adidas", size= 9):
+     
+        if isinstance(brand, str) and 1 <= len(brand) <= 25:
+            self._brand = brand
+        else:
+            print("Brand must be string between 1 and 100 characters.")
+            self._brand = "Unknown"
+
+        if isinstance(size, int) and size > 0:
+            self._size = size
+        else:
+            print("size must be an integer")
+            self._size = 6
+
+        self.condition= "Used"
+
+    def get_brand(self):
+        return self._brand
+
+    def set_brand(self, brand):
+        if isinstance(brand, str) and 1 <= len(brand) <= 25:
+            self._brand = brand
+        else:
+             print("Brand must be string between 1 and 25 characters.")
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+       if isinstance(size, int) and size > 0:
+            self._size = size
+       else:
+            print("size must be an integer")
+            self.size = 6
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+   
+    brand = property(get_brand, set_brand)
+    size = property(get_size, set_size)
+
+shoe = Shoe()
+print(shoe.brand)  
+print(shoe.size)  
+
+shoe.brand = "Air Force"
+shoe.size = 7
+print(shoe.brand)
+print(shoe.size)
+
+shoe.brand = "Nike"
+shoe.size = -3
+print(shoe.brand)
+print(shoe.size)
